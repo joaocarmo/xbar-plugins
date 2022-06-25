fff0dad95e622f526d1f67c59e5130aa25a03ac2
João Carmo <kingjoao@gmail.com>
2022-06-26 00:03:47 +0100

    Adds location option

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A collection of person plugins for [xbar][xbar], [SwiftBar][swiftbar], etc.
 
 ## Screenshot Settings
 
-Provides you with an easy way to change screenshot settings.
+Provides you with an easy way to change macOS' native screenshot settings.
 
 ![Screenshot Settings](./assets/screenshot-settings-preview.png)
 
--- src/screenshot-settings.sh
+++ src/screenshot-settings.sh
@@ -3,8 +3,8 @@
 # <bitbar.version>v1.0</bitbar.version>
 # <bitbar.author>João Carmo</bitbar.author>
 # <bitbar.author.github>joaocarmo</bitbar.author.github>
-# <bitbar.desc>Provides you with an easy way to change screenshot settings.</bitbar.desc>
-# <bitbar.image>https://raw.githubusercontent.com/joaocarmo/xbar-plugins/main/assets/screenshot-settings-preview.png</bitbar.image>
+# <bitbar.desc>Provides you with an easy way to change macOS' native screenshot settings.</bitbar.desc>
+# <bitbar.image>https://raw.githubusercontent.com/joaocarmo/xbar-plugins/main/assets/screenshot-settings.png</bitbar.image>
 # <bitbar.dependencies>bash</bitbar.dependencies>
 # <bitbar.abouturl>https://github.com/joaocarmo/xbar-plugins</bitbar.abouturl>
 # <swiftbar.hideAbout>false</swiftbar.hideAbout>
@@ -14,41 +14,58 @@
 # <swiftbar.hideSwiftBar>true</swiftbar.hideSwiftBar>
 # <swiftbar.refreshOnOpen>true</swiftbar.refreshOnOpen>
 
+FALSE="no"
+TRUE="yes"
+
+PNG="png"
+JPEG="jpg"
+
+DEFAULT_LOCATION="~/Desktop"
+
 DISABLE_SHADOWS=$(defaults read com.apple.screencapture disable-shadow 2>/dev/null)
 TYPE=$(defaults read com.apple.screencapture type 2>/dev/null)
+LOCATION=$(defaults read com.apple.screencapture location 2>/dev/null)
 
 # Set the default values if they are not set
 if [ -z "$DISABLE_SHADOWS" ]; then
-    DISABLE_SHADOWS="false"
+    DISABLE_SHADOWS=$FALSE
 fi
 
 if [ -z "$TYPE" ]; then
-    TYPE="png"
+    TYPE=$PNG
+fi
+
+if [ -z "$LOCATION" ]; then
+    LOCATION=$DEFAULT_LOCATION
 fi
 
 # Convert to bool
 if [ "$DISABLE_SHADOWS" = "1" ]; then
-    DISABLE_SHADOWS="true"
+    DISABLE_SHADOWS=$TRUE
 else
-    DISABLE_SHADOWS="false"
+    DISABLE_SHADOWS=$FALSE
 fi
 
 # Determine the new value
 if [ "$1" = "disable-shadows" ]; then
-    if [ "$DISABLE_SHADOWS" = "true" ]; then
-        DISABLE_SHADOWS="false"
+    if [ "$DISABLE_SHADOWS" = $TRUE ]; then
+        DISABLE_SHADOWS=$FALSE
     else
-        DISABLE_SHADOWS="true"
+        DISABLE_SHADOWS=$TRUE
     fi
     defaults write com.apple.screencapture disable-shadow -bool $DISABLE_SHADOWS
     killall SystemUIServer
 elif [ "$1" = "type" ]; then
-    if [ "$TYPE" = "png" ]; then
-        TYPE="jpg"
+    if [ "$TYPE" = $PNG ]; then
+        TYPE=$JPEG
     else
-        TYPE="png"
+        TYPE=$PNG
     fi
     defaults write com.apple.screencapture type $TYPE
+    killall SystemUIServer
+elif [ "$1" = "location" ]; then
+    LOCATION=$(osascript -e 'set theOutputFolder to POSIX path of (choose folder with prompt "Please select a screenshot output folder")')
+    defaults write com.apple.screencapture location $LOCATION
     killall SystemUIServer
 fi
 
@@ -60,3 +77,4 @@ echo ":camera:"
 echo "---"
 echo "Disable shadows: $DISABLE_SHADOWS | shell=$0 param1=disable-shadows terminal=false"
 echo "Save as type: $TYPE | shell=$0 param1=type terminal=false"
+echo "Save to: $LOCATION | shell=$0 param1=location terminal=false"
